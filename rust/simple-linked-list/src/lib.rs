eadb957418d72f418767887b548a9b3c743a0845
Eric Rodrigues Pires <eric@eric.dev.br>
2024-10-27 19:49:10 -0300

    Improved simple linked list

--- rust/simple-linked-list/src/lib.rs
+++ rust/simple-linked-list/src/lib.rs
@@ -1,12 +1,20 @@
+type Contents<T> = Option<(T, Box<Node<T>>)>;
+
+struct Node<T>(Contents<T>);
+
 pub struct SimpleLinkedList<T> {
     // Delete this field
     // dummy is needed to avoid unused parameter error during compilation
-    contents: Option<(T, Box<SimpleLinkedList<T>>)>,
+    contents: Contents<T>,
+    len: usize,
 }
 
 impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
-        Self { contents: None }
+        Self {
+            contents: None,
+            len: 0,
+        }
     }
 
     // You may be wondering why it's necessary to have is_empty()
@@ -19,22 +27,19 @@ impl<T> SimpleLinkedList<T> {
     }
 
     pub fn len(&self) -> usize {
-        match self.contents {
-            Some((_, ref tail)) => 1 + tail.len(),
-            None => 0,
-        }
+        self.len
     }
 
     pub fn push(&mut self, element: T) {
-        let contents = Box::new(Self {
-            contents: self.contents.take(),
-        });
+        let contents = Box::new(Node(self.contents.take()));
         self.contents = Some((element, contents));
+        self.len += 1;
     }
 
     pub fn pop(&mut self) -> Option<T> {
         let (element, contents) = self.contents.take()?;
-        self.contents = contents.contents;
+        self.contents = contents.0;
+        self.len -= 1;
         Some(element)
     }
 
@@ -44,10 +49,11 @@ impl<T> SimpleLinkedList<T> {
 
     #[must_use]
     pub fn rev(self) -> SimpleLinkedList<T> {
-        let mut this = self;
         let mut list = Self::new();
-        while let Some(element) = this.pop() {
+        let mut node = self.contents;
+        while let Some((element, new_node)) = node {
             list.push(element);
+            node = new_node.0;
         }
         list
     }
@@ -75,18 +81,19 @@ impl<T> FromIterator<T> for SimpleLinkedList<T> {
 // of the vector as far as the tests are concerned.
 
 impl<T> From<SimpleLinkedList<T>> for Vec<T> {
-    fn from(mut linked_list: SimpleLinkedList<T>) -> Vec<T> {
+    fn from(linked_list: SimpleLinkedList<T>) -> Vec<T> {
         let len = linked_list.len();
-        let vec = Vec::with_capacity(len);
-        let mut vec = std::mem::ManuallyDrop::new(vec);
+        let mut vec = std::mem::ManuallyDrop::new(Vec::with_capacity(len));
         let cap = vec.capacity();
         let vec_ptr: *mut T = vec.as_mut_ptr();
+        let mut node = linked_list.contents;
         for i in (0..len).rev() {
-            let element = linked_list.pop().unwrap();
+            let (element, new_node) = node.expect("len should match number of elements");
             // SAFETY: We only access the allocated capacity of the underlying Vec (i.e. in range 0..len).
             unsafe {
                 *vec_ptr.add(i) = element;
             }
+            node = new_node.0;
         }
         // SAFETY: We are reusing values from the previous Vec allocation.
         // Conversely, ManuallyDrop guarantees that there won't be a double-free.
